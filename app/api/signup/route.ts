9fa7cb6e0e01c26d6f3fa734f1173ca2720caf40
UjjwalArote69 <ujjwalarote69@gmail.com>
2026-03-14 14:45:20 +0530

    findone to findunique

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: Request) {
             return new NextResponse("Missing required fields", {status: 400});
         }
 
-        const existingUser = await prisma.user.findOne({
+        const existingUser = await prisma.user.findUnique({
             where:{email},
         });
 
